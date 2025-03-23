a0f95ef716e3bfc17ad97b10ce6bbe4bde5b0bf8
shmayzer <borisenkomaksim10@gmail.com>
2025-03-23 20:35:10 +0100

    Add practice site (PR_1 & PR_2)

---
+++ js/load-header-footer.js
@@ -0,0 +1,38 @@
+const basePath = window.location.hostname.includes('github.io')
+  ? '/practice/'
+  : '/'
+
+document.addEventListener('DOMContentLoaded', function () {
+  fetch(basePath + 'header.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('header').innerHTML = data
+    })
+    .catch(error => console.error('Error loading header:', error))
+
+  fetch(basePath + 'footer.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('footer').innerHTML = data
+    })
+    .catch(error => console.error('Error loading footer:', error))
+
+  document.querySelectorAll('link[rel="stylesheet"]').forEach(link => {
+    if (link.href.includes('https://') || link.href.includes('http://')) {
+      return
+    }
+    link.href = basePath + link.getAttribute('href')
+  })
+
+  document.querySelectorAll('script').forEach(script => {
+    if (
+      script.src &&
+      (script.src.includes('https://') || script.src.includes('http://'))
+    ) {
+      return
+    }
+    if (script.src) {
+      script.src = basePath + script.getAttribute('src')
+    }
+  })
+})
